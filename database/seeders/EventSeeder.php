6b03a2605a9b31509db77b11ceaf142fd9776c0b
Jeremi Olczak <n00230483@iadt.ie>
2024-10-22 21:39:48 +0100

    Changing content of EventSeeder to include event_time and give it a pre-definition

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -1,20 +1,44 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Carbon\Carbon;
-
+use App\Models\Event;
 
 class EventSeeder extends Seeder
 {
     public function run()
     {
         Event::insert([
-            ['id' => 1, 'name' => 'Beethoven Concert', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'name' => 'Mozart Concert', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 3, 'name' => 'Bach Concert', 'created_at' => now(), 'updated_at' => now()],
+            [
+                'name' => 'Beethoven Concert',
+                'event_date' => '2024-10-22', 
+                'start_time' => '19:00:00',  
+                'end_time' => '21:00:00',     
+                'location' => 'Concert Hall',  
+                'description' => 'A night of Beethoven’s greatest hits.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Mozart Concert',
+                'event_date' => '2024-11-05', 
+                'start_time' => '18:00:00',   
+                'end_time' => '20:00:00',     
+                'location' => 'Opera House',   
+                'description' => 'An evening of Mozart’s masterpieces.', 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Bach Concert',
+                'event_date' => '2024-12-10', 
+                'start_time' => '20:00:00',   
+                'end_time' => '22:00:00',     
+                'location' => 'Symphony Center', 
+                'description' => 'A celebration of Bach’s works.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
